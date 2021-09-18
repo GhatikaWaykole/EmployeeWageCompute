3d204307730ebf23b41918342e39f383a2160a70
ghatikawaykole <ghatika99@gmail.com>
2021-09-18 16:11:24 +0530

    Added the random to check employee present or absent

--- src/com/example/EmployeeWageCompute/EmployeewageCompute.java
+++ src/com/example/EmployeeWageCompute/EmployeewageCompute.java
@@ -1,7 +1,20 @@
 package com.example.EmployeeWageCompute;
 
+import java.util.Random;
+
 public class EmployeewageCompute {
     public static void main(String args[]){
         System.out.println("Welcome to the employeewage compute");
+        // declare the constant in the program
+        int IS_FULL_TIME=1;
+        // random to compute the value of random number rang of 2 number
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+            }
     }
 }
